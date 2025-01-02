cecdf78fae8edbbad526bb45564078e8606f6150
mreshboboyev <mreshboboyev@gmail.com>
2025-01-02 18:35:09 +0500

    Create Product model; Update Function.cs for add product to table;

--- ProductApi/Function.cs
+++ ProductApi/Function.cs
@@ -1,5 +1,9 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using System.Text.Json;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using ProductApi.Models;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -7,9 +11,44 @@ namespace ProductApi;
 
 public class Function
 {
-    public async Task<APIGatewayProxyResponse> FunctionHandler(Function function)
+    private static readonly IAmazonDynamoDB _client = 
+            new AmazonDynamoDBClient(
+                Environment.GetEnvironmentVariable("AccessKey"),
+                Environment.GetEnvironmentVariable("SecretKey"),
+            new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.USEast1
+            });
+    
+    private static readonly Table _table = new TableBuilder(_client, "Products")
+        .AddHashKey("ProductId", DynamoDBEntryType.String)
+        .Build();
+    
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request)
     {
         Console.WriteLine("FunctionHandler called");
+        
+        var product = JsonSerializer.Deserialize<Product>(request.Body);
+
+        if (product == null)
+        {
+            return new APIGatewayProxyResponse()
+            {
+                StatusCode = 400,
+                Body = "Invalid product data",
+            };
+        }
+        
+        var document = new Document()
+        {
+            ["ProductId"] = product.ProductId,
+            ["Name"] = product.Name,
+            ["Price"] = product.Price
+        };
+        
+        await _table.PutItemAsync(document);
+
+        Console.WriteLine("Product added successfully!");
 
         return new APIGatewayProxyResponse()
         {
---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductApi.Models;
+
+public class Product
+{
+    public string ProductId { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
