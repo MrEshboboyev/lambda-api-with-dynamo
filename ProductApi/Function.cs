92d6b863e9f8bd4105a8eca43ae914f1b895bcf8
mreshboboyev <mreshboboyev@gmail.com>
2025-01-02 18:09:02 +0500

    Create FunctionHandler() method with LambdaSerializer

---
+++ ProductApi/Function.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ProductApi;
+
+public class Function
+{
+    public async Task<APIGatewayProxyResponse> FunctionHandler(Function function)
+    {
+        Console.WriteLine("FunctionHandler called");
+
+        return new APIGatewayProxyResponse()
+        {
+            StatusCode = 200,
+            Body = "Product added successfully!"
+        };
+    }
+}
